6a01c42d93a48e7c151920535992c347b3374d85
lahirumw <lahirumw@gmail.com>
2016-03-23 22:43:01 +0530

    second revision add comments to ResponseObject

--- cassendra-crud/src/main/java/com/lah/smgt/shared/ResponseObject.java
+++ cassendra-crud/src/main/java/com/lah/smgt/shared/ResponseObject.java
@@ -1,12 +1,23 @@
 package com.lah.smgt.shared;
 
+/*
+ * initiate by LahiruA
+ * This is the response object for every HTTP method
+ * 
+ */
+
 import java.util.List;
 import java.util.Map;
 
 public class ResponseObject {
 
+	//for listing errors
 	private List<Error> errList;
+	
+	//for listing requested data
 	private List<?> data;
+	
+	//for bring successful message
 	private Map<String, String> msg;
 	
 	public List<Error> getErrList() {
